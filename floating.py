99dd5ebf1a08b6619fde259f54deeb19f19528e5
mddeloarhosen68 <62032396+mddeloarhosen68@users.noreply.github.com>
2020-05-08 03:00:41 +0600

    Add files via upload

---
+++ floating.py
@@ -0,0 +1,18 @@
+#Ex:1
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#EX:2
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ type_conversion.py
@@ -0,0 +1,7 @@
+x = float(1)       #convert from int to float:
+y = int(2.8)       #convert from float to int:
+z = complex(x)     #convert from int to complex:
+
+print(x)
+print(y)
+print(z)
